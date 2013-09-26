c4fc75008fb29bec8f149efeeaf1ae1154cc96d6
Pietro Zambelli <peter.zamb@gmail.com>
2013-09-26 23:13:44 +0100

    reduce the lenght of the code lines

--- xdress/base.py
+++ xdress/base.py
@@ -9,6 +9,7 @@ Base Plugin API
 """
 from __future__ import print_function
 import os
+from os.path import join, isdir, sep
 import sys
 
 from .utils import RunControl, NotSpecified, writenewonly, DescriptionCache, \
@@ -18,6 +19,7 @@ from .typesystem import TypeSystem
 
 if sys.version_info[0] >= 3:
     basestring = str
+
 
 class XDressPlugin(Plugin):
     """This class provides base functionality for xdress itself."""
@@ -42,11 +44,11 @@ class XDressPlugin(Plugin):
     rcdocs = {
         'rc': "Path to run control file",
         'plugins': "Plugins to include",
-        'debug': 'Build in debugging mode', 
+        'debug': 'Build in debugging mode',
         'ts': "The xdress type system.",
         'verbose': "Print more output.",
         'dumpdesc': "Print the description cache",
-        'package': "The Python package name for the generated wrappers", 
+        'package': "The Python package name for the generated wrappers",
         'packagedir': "Path to package directory, same as 'package' if not specified",
         'sourcedir': "Path to source directory",
         'builddir': "Path to build directory",
@@ -56,12 +58,15 @@ class XDressPlugin(Plugin):
 
     def update_argparser(self, parser):
         parser.add_argument('--rc', help=self.rcdocs['rc'])
-        parser.add_argument('--plugins', nargs="+", help=self.rcdocs["plugins"])
-        parser.add_argument('--debug', action='store_true', 
+        parser.add_argument('--plugins', nargs="+",
+                            help=self.rcdocs["plugins"])
+        parser.add_argument('--debug', action='store_true',
                             help=self.rcdocs["debug"])
-        parser.add_argument('-v', '--verbose', action='store_true', dest='verbose',
+        parser.add_argument('-v', '--verbose', action='store_true',
+                            dest='verbose',
                             help=self.rcdocs["verbose"])
-        parser.add_argument('--dumpdesc', action='store_true', dest='dumpdesc',
+        parser.add_argument('--dumpdesc', action='store_true',
+                            dest='dumpdesc',
                             help=self.rcdocs["dumpdesc"])
         parser.add_argument('--package', action='store', dest='package',
                             help=self.rcdocs["package"])
@@ -72,25 +77,27 @@ class XDressPlugin(Plugin):
         parser.add_argument('--builddir', action='store', dest='builddir',
                             help=self.rcdocs["builddir"])
         parser.add_argument('--bash-completion', action='store_true',
-                            help="enable bash completion", dest="bash_completion")
+                            help="enable bash completion",
+                            dest="bash_completion")
         parser.add_argument('--no-bash-completion', action='store_false',
-                            help="disable bash completion", dest="bash_completion")
+                            help="disable bash completion",
+                            dest="bash_completion")
 
     def setup(self, rc):
         if rc.package is NotSpecified:
             msg = "no package name given; please add 'package' to {0}"
             sys.exit(msg.format(rc.rc))
         if rc.packagedir is NotSpecified:
-            rc.packagedir = rc.package.replace('.', os.path.sep)
-        if not os.path.isdir(rc.packagedir):
+            rc.packagedir = rc.package.replace('.', sep)
+        if not isdir(rc.packagedir):
             os.makedirs(rc.packagedir)
-        if not os.path.isdir(rc.sourcedir):
+        if not isdir(rc.sourcedir):
             os.makedirs(rc.sourcedir)
-        if not os.path.isdir(rc.builddir):
+        if not isdir(rc.builddir):
             os.makedirs(rc.builddir)
-        writenewonly("", os.path.join(rc.packagedir, '__init__.py'), rc.verbose)
-        writenewonly("", os.path.join(rc.packagedir, '__init__.pxd'), rc.verbose)
-        rc._cache = DescriptionCache(cachefile=os.path.join(rc.builddir, 'desc.cache'))
+        writenewonly("", join(rc.packagedir, '__init__.py'), rc.verbose)
+        writenewonly("", join(rc.packagedir, '__init__.pxd'), rc.verbose)
+        rc._cache = DescriptionCache(cachefile=join(rc.builddir, 'desc.cache'))
 
         if rc.dumpdesc:
             print(str(rc._cache))
@@ -102,4 +109,3 @@ class XDressPlugin(Plugin):
         msg += nyansep + "\n\n"
         msg += "Current type system contents:\n\n" + str(rc.ts) + "\n\n"
         return msg
-
