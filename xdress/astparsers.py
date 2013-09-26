4edb305a9e9a2315a07d0312a14cb5e715f059b4
Pietro Zambelli <peter.zamb@gmail.com>
2013-09-26 22:26:37 +0100

    Remove trailing spase and add spaces where needed to be compliant to the pep8

--- xdress/astparsers.py
+++ xdress/astparsers.py
@@ -1,4 +1,4 @@
-"""This module creates abstract syntax trees using external tools 
+"""This module creates abstract syntax trees using external tools
 (GCC-XML, pycparser) of C/C++ code.
 
 :author: Anthony Scopatz <scopatz@gmail.com>
@@ -66,7 +66,7 @@ from .utils import guess_language, RunControl, NotSpecified
 from .plugins import Plugin
 
 PARSERS_AVAILABLE = {
-    'clang': False, 
+    'clang': False,
     'pycparser': pycparser is not None,
     }
 with tempfile.NamedTemporaryFile() as f:
@@ -82,8 +82,9 @@ with tempfile.NamedTemporaryFile() as f:
         PARSERS_AVAILABLE['gccxml'] = False
 del f
 
-if sys.version_info[0] >= 3: 
+if sys.version_info[0] >= 3:
     basestring = str
+
 
 def _makekey(obj):
     if isinstance(obj, basestring):
@@ -93,20 +94,23 @@ def _makekey(obj):
     elif isinstance(obj, collections.Set):
         return frozenset([_makekey(o) for o in obj])
     elif isinstance(obj, collections.Mapping):
-        return tuple([(_makekey(k), _makekey(v)) for k, v in sorted(obj.items())])
+        return tuple([(_makekey(k), _makekey(v))
+                      for k, v in sorted(obj.items())])
     else:
         return obj
+
 
 def _memoize_parser(f):
     # based off code from http://wiki.python.org/moin/PythonDecoratorLibrary
     cache = f.cache = {}
+
     @functools.wraps(f)
     def memoizer(*args, **kwargs):
         key = _makekey(args) + _makekey(kwargs)
         if key in cache:
             value = cache[key]
         else:
-            value = f(*args, **kwargs) 
+            value = f(*args, **kwargs)
             try:
                 cache[key] = value
             except TypeError:
@@ -114,11 +118,13 @@ def _memoize_parser(f):
         return value
     return memoizer
 
+
 def clearmemo():
     """Clears all function memoizations for autodescribers."""
     for x in globals().values():
         if callable(x) and hasattr(x, 'cache'):
             x.cache.clear()
+
 
 def not_implemented(obj):
     if not isinstance(obj, type):
@@ -126,6 +132,7 @@ def not_implemented(obj):
             obj.__doc__ = ''
         obj.__doc__ += ("\n\n.. warning:: This has not yet been implemented "
                         "fully or at all.\n\n")
+
     @functools.wraps(obj)
     def func(*args, **kwargs):
         msg = "The functionality in {0} has not been implemented fully or at all"
@@ -133,13 +140,14 @@ def not_implemented(obj):
         raise NotImplementedError(msg)
     return func
 
+
 #
 # GCC-XML Describers
 #
 
 @_memoize_parser
-def gccxml_parse(filename, includes=(), defines=('XDRESS',), undefines=(), 
-                  verbose=False, debug=False, builddir='build'):
+def gccxml_parse(filename, includes=(), defines=('XDRESS',), undefines=(),
+                 verbose=False, debug=False, builddir='build'):
     """Use GCC-XML to parse a file. This function is automatically memoized.
 
     Parameters
@@ -182,14 +190,15 @@ def gccxml_parse(filename, includes=(), defines=('XDRESS',), undefines=(),
     f.close()
     return root
 
+
 #
 # pycparser Describers
 #
 
 @_memoize_parser
-def pycparser_parse(filename, includes=(), defines=('XDRESS',), undefines=(), 
+def pycparser_parse(filename, includes=(), defines=('XDRESS',), undefines=(),
                     verbose=False, debug=False, builddir='build'):
-    """Use pycparser to parse a file.  This functions is automatically memoized.
+    """Use pycparser to parse a file. This functions is automatically memoized.
 
     Parameters
     ----------
@@ -214,18 +223,18 @@ def pycparser_parse(filename, includes=(), defines=('XDRESS',), undefines=(),
         A pycparser abstract syntax tree.
 
     """
-    pklgzname = filename.replace(os.path.sep, '_').rsplit('.', 1)[0] + '.pkl.gz'
+    ext = '.pkl.gz'
+    pklgzname = filename.replace(os.path.sep, '_').rsplit('.', 1)[0] + ext
     pklgzname = os.path.join(builddir, pklgzname)
     if os.path.isfile(pklgzname):
         with gzip.open(pklgzname, 'rb') as f:
             root = pickle.loads(f.read())
         return root
     kwargs = {'cpp_args': [r'-D__attribute__(x)=',  # Workaround for GNU libc
-                r'-D__asm__(x)=', r'-D__const=', 
-                r'-D__builtin_va_list=int', # just fake this
-                r'-D__restrict=', r'-D__extension__=', 
-                r'-D__inline__=', r'-D__inline=',
-                ]}
+                           r'-D__asm__(x)=', r'-D__const=',
+                           r'-D__builtin_va_list=int',  # just fake this
+                           r'-D__restrict=', r'-D__extension__=',
+                           r'-D__inline__=', r'-D__inline=', ]}
     kwargs['cpp_args'] += ['-I' + i for i in includes]
     kwargs['cpp_args'] += ['-D' + d for d in defines]
     kwargs['cpp_args'] += ['-U' + d for u in undefines]
@@ -233,6 +242,7 @@ def pycparser_parse(filename, includes=(), defines=('XDRESS',), undefines=(),
     with gzip.open(pklgzname, 'wb') as f:
         f.write(pickle.dumps(root, pickle.HIGHEST_PROTOCOL))
     return root
+
 
 #
 #  General utilities
@@ -246,11 +256,12 @@ def pick_parser(filename, parsers):
     filename : str
         The path to the file.
     parsers : str, list, or dict, optional
-        The parser / AST to use to use for the file.  Currently 'clang', 'gccxml', 
-        and 'pycparser' are supported, though others may be implemented in the 
-        future.  If this is a string, then this parser is used.  If this is a list, 
-        this specifies the parser order to use based on availability.  If this is
-        a dictionary, it specifies the order to use parser based on language, i.e.
+        The parser / AST to use to use for the file.
+        Currently 'clang', 'gccxml', and 'pycparser' are supported, though
+        others may be implemented in the future.  If this is a string, then
+        this parser is used.  If this is a list, this specifies the parser
+        order to use based on availability.  If this is a dictionary, it
+        specifies the order to use parser based on language, i.e.
         ``{'c' ['pycparser', 'gccxml'], 'c++': ['gccxml', 'pycparser']}``.
 
     Returns
@@ -280,6 +291,7 @@ def pick_parser(filename, parsers):
         raise ValueError("type of parsers not intelligible")
     return parser
 
+
 def _pformat_etree_inplace(elem, level=0):
     i = "\n" + level*"  "
     if len(elem):
@@ -295,7 +307,8 @@ def _pformat_etree_inplace(elem, level=0):
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = i
 
-def dumpast(filename, parsers, sourcedir, includes=(), defines=('XDRESS',), 
+
+def dumpast(filename, parsers, sourcedir, includes=(), defines=('XDRESS',),
             undefines=(), verbose=False, debug=False, builddir='build'):
     """Prints an abstract syntax tree to stdout."""
     if not os.path.isfile(filename):
@@ -304,13 +317,13 @@ def dumpast(filename, parsers, sourcedir, includes=(), defines=('XDRESS',),
             sys.exit(filename + " is not a regular file")
     parser = pick_parser(filename, parsers)
     if parser == 'pycparser':
-        root = pycparser_parse(filename, includes=includes, defines=defines, 
-                               undefines=undefines, verbose=verbose, debug=debug, 
-                               builddir=builddir)
+        root = pycparser_parse(filename, includes=includes, defines=defines,
+                               undefines=undefines, verbose=verbose,
+                               debug=debug, builddir=builddir)
         root.show()
     elif parser == 'gccxml':
-        root = gccxml_parse(filename, includes=includes, defines=defines, 
-                            undefines=undefines, verbose=verbose, debug=debug, 
+        root = gccxml_parse(filename, includes=includes, defines=defines,
+                            undefines=undefines, verbose=verbose, debug=debug,
                             builddir=builddir)
         if HAVE_LXML:
             print(etree.tostring(root, pretty_print=True))
@@ -319,8 +332,7 @@ def dumpast(filename, parsers, sourcedir, includes=(), defines=('XDRESS',),
             print(etree.tostring(root))
     else:
         sys.exit(parser + " is not a valid parser")
-    
-    
+
 
 #
 # Plugin
@@ -332,7 +344,7 @@ class ParserPlugin(Plugin):
 
     requires = ('xdress.base',)
     """This plugin requires 'xdress.base'."""
-    
+
     defaultrc = utils.RunControl(
         includes=[],
         defines=["XDRESS"],
@@ -340,8 +352,8 @@ class ParserPlugin(Plugin):
         variables=(),
         functions=(),
         classes=(),
-        parsers={'c': ['pycparser', 'gccxml', 'clang'],
-                 'c++':['gccxml', 'clang', 'pycparser']},
+        parsers={'c':   ['pycparser', 'gccxml', 'clang'],
+                 'c++': ['gccxml', 'clang', 'pycparser']},
         clear_parser_cache_period=50,
         dumpast=NotSpecified,
         )
@@ -349,7 +361,7 @@ class ParserPlugin(Plugin):
     rcupdaters = {'includes': lambda old, new: list(new) + list(old)}
 
     rcdocs = {
-        'includes': "Additional include directories", 
+        'includes': "Additional include directories",
         'defines': "Set additional macro definitions",
         'undefines': "Unset additional macro definitions",
         'variables': ("A list of variable names in sequence, mapping, "
@@ -359,42 +371,49 @@ class ParserPlugin(Plugin):
         'classes': ("A list of class names in sequence, mapping, "
                     "or apiname format"),
         'parsers': "Parser(s) name, list, or dict",
-        'clear_parser_cache_period': ("Number of parser calls to perform before "
-                                      "clearing the internal cache.  This prevents "
-                                      "nasty memory overflow issues."),
-        'dumpast': "Prints the abstract syntax tree of a file.", 
+        'clear_parser_cache_period': ("Number of parser calls to perform"
+                                      " before clearing the internal cache. "
+                                      "This prevents nasty memory overflow "
+                                      "issues."),
+        'dumpast': "Prints the abstract syntax tree of a file.",
         }
 
     def update_argparser(self, parser):
         rcdocs = self.rcdocs() if callable(self.rcdocs) else self.rcdocs
-        parser.add_argument('-I', '--includes', action='store', dest='includes', 
+        parser.add_argument('-I', '--includes', action='store',
+                            dest='includes',
                             nargs="+", help=rcdocs["includes"])
-        parser.add_argument('-D', '--defines', action='append', dest='defines', 
+        parser.add_argument('-D', '--defines', action='append',
+                            dest='defines',
                             nargs="+", help=rcdocs["defines"])
-        parser.add_argument('-U', '--undefines', action='append', dest='undefines',
+        parser.add_argument('-U', '--undefines', action='append',
+                            dest='undefines',
                             nargs="+", type=str, help=rcdocs["undefines"])
-        parser.add_argument('-p', action='store', dest='parsers', 
+        parser.add_argument('-p', action='store',
+                            dest='parsers',
                             help=rcdocs["parsers"])
-        parser.add_argument('--clear-parser-cache-period', action='store', 
+        parser.add_argument('--clear-parser-cache-period', action='store',
                             dest='clear_parser_cache_period', type=int,
                             help=rcdocs["clear_parser_cache_period"])
-        parser.add_argument('--dumpast', action='store', dest='dumpast', 
+        parser.add_argument('--dumpast', action='store',
+                            dest='dumpast',
                             metavar="FILE", help=rcdocs["dumpast"])
 
     def setup(self, rc):
         """Remember to call super() on subclasses!"""
         if isinstance(rc.parsers, basestring):
-            if '[' in rc.parsers or '{' in  rc.parsers:
+            if '[' in rc.parsers or '{' in rc.parsers:
                 rc.parsers = eval(rc.parsers)
         # This should go last
         if rc.dumpast is not NotSpecified:
-            dumpast(rc.dumpast, rc.parsers, rc.sourcedir, includes=rc.includes, 
+            dumpast(rc.dumpast, rc.parsers, rc.sourcedir, includes=rc.includes,
                     defines=rc.defines, undefines=rc.undefines,
                     verbose=rc.verbose, debug=rc.debug, builddir=rc.builddir)
             sys.exit()
 
     def execute(self, rc):
-        raise TypeError("ParserPlugin is not a complete plugin.  Do not use directly")
+        msg = "ParserPlugin is not a complete plugin.  Do not use directly"
+        raise TypeError(msg)
 
     def report_debug(self, rc):
         """Remember to call super() on subclasses!"""
